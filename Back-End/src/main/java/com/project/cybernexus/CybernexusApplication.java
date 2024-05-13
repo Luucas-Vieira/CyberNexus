5b2913a4fdb1a6cab5fa1833865b079b30181834
Luciano Lucas Vieira Da Silva <lucianolucas1677@gmail.com>
2024-05-13 11:36:14 -0300

    Update CybernexusApplication.java

--- Back-End/src/main/java/com/project/cybernexus/CybernexusApplication.java
+++ Back-End/src/main/java/com/project/cybernexus/CybernexusApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CybernexusApplication {
 
+        @Value("${PORT:8080}")
+        private int port;
+
     public static void main(String[] args) {
         SpringApplication.run(CybernexusApplication.class, args);
         System.out.println("Cybernexus Application Started");
